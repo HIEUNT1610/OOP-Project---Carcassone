c42521a45a567876aafc83efdc771df92f746cbe
HD1225 <houd0816@gmail.com>
2022-12-27 20:21:26 +0100

    add player

--- src/dominos/Game.java
+++ src/dominos/Game.java
@@ -3,6 +3,12 @@ package dominos;
 public class Game {
     private Board board;
     private Bag bag;
+    private Player[] players;
+
+    public Game(int nbPlayer) {
+        players = new Player[nbPlayer];
+
+    }
 
     //The turn system probably needs to be in here.
 
