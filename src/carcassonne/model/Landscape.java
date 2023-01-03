bc1cb2a25463fb924ed948b4c328e959d9e6b8ba
HD1225 <houd0816@gmail.com>
2023-01-03 18:15:11 +0100

    Landscape finish

--- src/carcassonne/model/Landscape.java
+++ src/carcassonne/model/Landscape.java
@@ -4,8 +4,27 @@ public enum Landscape {
     A(9, Side.LAWN, Side.ROAD, Side.ROAD, Side.LAWN),
     B(3, Side.TOWN, Side.LAWN, Side.ROAD, Side.ROAD),
     C(2, Side.TOWN, Side.TOWN, Side.ROAD, Side.ROAD),
-    D(1, Side.TOWN, Side.TOWN, Side.ROAD, Side.TOWN)
-//    E(1, Side.TOWN, Side.TOWN, )
+    D(1, Side.TOWN, Side.TOWN, Side.ROAD, Side.TOWN),
+    E(1, Side.TOWN, Side.TOWN, Side.LAWN, Side.TOWN),
+    F(3, Side.TOWN, Side.TOWN, Side.ROAD, Side.ROAD),
+    G(3, Side.TOWN, Side.ROAD, Side.ROAD, Side.ROAD),
+    H(8, Side.ROAD, Side.LAWN, Side.ROAD, Side.LAWN),
+    I(4, Side.LAWN, Side.ROAD, Side.ROAD, Side.ROAD),
+    J(5, Side.TOWN, Side.LAWN, Side.LAWN, Side.LAWN),
+    K(2, Side.TOWN, Side.LAWN, Side.LAWN, Side.TOWN),
+    L(3, Side.TOWN, Side.TOWN, Side.LAWN, Side.TOWN),
+    M(4, Side.LAWN, Side.LAWN, Side.LAWN, Side.LAWN),
+    N(2, Side.LAWN, Side.LAWN, Side.ROAD, Side.LAWN),
+    O(3, Side.TOWN, Side.TOWN, Side.LAWN, Side.LAWN),
+    P(2, Side.LAWN, Side.TOWN, Side.LAWN, Side.TOWN),
+    Q(4, Side.TOWN, Side.ROAD, Side.LAWN, Side.ROAD),
+    R(3, Side.TOWN, Side.ROAD, Side.ROAD, Side.LAWN),
+    S(1, Side.ROAD, Side.TOWN, Side.ROAD, Side.TOWN),
+    T(2, Side.TOWN, Side.TOWN, Side.ROAD, Side.TOWN),
+    U(1, Side.TOWN, Side.TOWN, Side.TOWN, Side.TOWN),
+    V(1, Side.ROAD, Side.ROAD, Side.ROAD, Side.ROAD),
+    W(2, Side.TOWN, Side.TOWN, Side.LAWN, Side.LAWN),
+    X(3, Side.LAWN, Side.TOWN, Side.LAWN, Side.TOWN);
 
 
     private Side[] sides;
